1c4a9a450420f109478baa1118941975ca52a88b
dongwq <dongwqs@gmail.com>
2013-09-30 09:26:46 +0800

    add web server, add the file

--- demos/TestVerticle3.java
+++ demos/TestVerticle3.java
@@ -52,4 +52,4 @@ public class TestVerticle3 extends Verticle {
       }
     });
   }
-}
+}	
---
+++ demos/WebServer.java
@@ -0,0 +1,17 @@
+// WebSereer.java
+
+
+import org.vertx.java.core.Handler;
+  import org.vertx.java.core.http.HttpServerRequest;
+  import org.vertx.java.platform.Verticle;
+
+  public class WebServer extends Verticle {
+      public void start() {
+          vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+              public void handle(HttpServerRequest req) {
+                  String file = req.path().equals("/") ? "index.html" : req.path();
+                  req.response().sendFile("webroot/" + file);
+              }
+          }).listen(8080);
+      }
+  }
